e7adf91574cc93bdfa3e55dbf20712dc78d6de7c
Gongarci <gongarci42madrid@42madrid.com>
2025-07-31 19:56:32 +0200

    update

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -11,9 +11,58 @@ Fixed::~Fixed()
 	std::cout << "Destructor called" << std::endl;
 }
 
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits  member function called" << std::endl;
+	return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return static_cast<float>(this->_value) / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+	return this->_value >> _fractionalBits;
+}
+
 Fixed&::min(Fixed& a, Fixed& b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+Fixed&::min(Fixed& const a, Fixed& const b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed&::max(Fixed& a, Fixed& b)
 {
 	if (a > b)
 		return (a);
 	return (b);
 }
+
+Fixed&::max(Fixed& const a, Fixed& const b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+bool Fixed::operator<(const Fixed& fixed){return (this->_value < fixed._value);}
+bool Fixed::operator<=(const Fixed& fixed){return (this->_value <= fixed._value);}
+bool Fixed::operator>(const Fixed& fixed){return (this->_value > fixed._value);}
+bool Fixed::operator>=(const Fixed& fixed){return (this->_value >= fixed._value);}
+bool Fixed::operator==(const Fixed& fixed){return (this->_value == fixed._value);}
+bool Fixed::operator!=(const Fixed& fixed){return (this->_value != fixed._value);}
+
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,18 +6,43 @@
 
 class Fixed
 {
+	private:
+		int	_value;
+		static const int _fractionalBits = 8;
+
 	public:
 		Fixed();
 		Fixed(const Fixed &other); //coppy constructor
 		Fixed &operator=(const Fixed &other); //assignment operator
 		~Fixed();
 		friend std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+		
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 
-		Fixed& min(Fixed& a, Fixed& b);
+		static Fixed&	min(Fixed& a, Fixed& b);
+		static Fixed&	min(Fixed& const a, Fixed& const b);
+		static Fixed&	max(Fixed&  a, Fixed&  b);
+		static Fixed&	max(Fixed& const a, Fixed& const b);
 
-	private:
-		int	_value;
-		static const int _fractionalBits = 8;
+
+
+		/* Comparison operators */
+		bool operator<(const Fixed& fixed);
+		bool operator<=(const Fixed& fixed);
+		bool operator>(const Fixed& fixed);
+		bool operator>=(const Fixed& fixed);
+		bool operator==(const Fixed& fixed);
+		bool operator!=(const Fixed& fixed);
+		/* voerload Aritmethic Operators */
+		Fixed operator+(const Fixed&) const;
+		Fixed operator-(const Fixed&) const;
+		Fixed operator/(const Fixed&) const;
+		Fixed operator*(const Fixed&) const;
+
+
 };
 
 #endif
