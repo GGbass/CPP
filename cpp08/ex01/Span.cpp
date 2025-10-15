7dccbf993346d508bdad61d3d48d6c652bdee8d0
Gongarci <gongarci42madrid@42madrid.com>
2025-10-15 21:58:44 +0200

    update

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,11 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gongarci <gongarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 20:41:13 by gongarci          #+#    #+#             */
+/*   Updated: 2025/10/15 20:50:21 by gongarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Span.hpp"
 
-Span::Span()
+Span::Span(void) : _n(0)
 {
-	// Constructor implementation
+	std::cout << "Decault Span constructor called" << std::endl;
 }
+
+Span::Span(unsigned int n) : _n(n)
+{
+	std::cout << "Span constructor called" << std::endl;
+}
+
+Span::Span(const Span &other)
+{
+	if (this != &other)
+	{
+		this->_vect = (other._vect);
+		std::cout << "Copy constructor called" << std::endl;
+	}
+}
+
+Span& Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		this->_vect = (other._vect);
+		std::cout << "Asssigning operator called" << std::endl;
+	}
+}
+
 
 Span::~Span()
 {
-	// Destructor implementation
+	std::cout << "Destructor called" << std::endl;
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,10 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gongarci <gongarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 20:41:07 by gongarci          #+#    #+#             */
+/*   Updated: 2025/10/15 20:45:49 by gongarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef Span_HPP
 #define Span_HPP
+#include <iostream>
+#include <vector>
+
 
 class Span
 {
 	private:
-		unsigned int *_n;
+		unsigned int			_n;
+		std::vector<int>		_vect;
 
 	public:
 		Span();
@@ -13,7 +29,7 @@ class Span
 		Span& operator=(const Span &other);
 		~Span();
 		/* methods */
-		void	addNumber(unsigned int n);
+		void	addNumbers(unsigned int n);
 };
 
 #endif // Span_HPP
